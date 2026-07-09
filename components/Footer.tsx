d37f15ec514ac3d13b96d3724a2950e2add660da
Fumiaki <mitsubachi.fruits@gmail.com>
2026-07-09 18:35:46 +0900

    add legal link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,36 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer
-      id="contact"
       style={{
-        background: "#f2f2ef",
-        padding: "100px 0",
-        marginTop: "120px",
+        background: "#fafaf9",
+        borderTop: "1px solid #ecece8",
+        padding: "48px 24px",
+        marginTop: "80px",
       }}
     >
-      <div className="container">
-        <h2
+      <div
+        style={{
+          maxWidth: "1100px",
+          margin: "0 auto",
+          display: "flex",
+          flexDirection: "column",
+          gap: "24px",
+          alignItems: "center",
+          textAlign: "center",
+        }}
+      >
+        {/* ロゴ */}
+        <Link
+          href="/"
           style={{
-            display: "flex",
-            fontSize: "48px",
-            marginBottom: "40px",
+            display: "inline-flex",
+            alignItems: "center",
+            fontSize: "28px",
+            fontWeight: 900,
+            letterSpacing: "0em",
+            textDecoration: "none",
           }}
         >
           <span style={{ color: "#FF3131" }}>み</span>
@@ -23,22 +40,57 @@ export default function Footer() {
           <span style={{ color: "#8C52FF" }}>果</span>
           <span style={{ color: "#00A1FF" }}>樹</span>
           <span style={{ color: "#FF66C4" }}>園</span>
-        </h2>
+        </Link>
 
-        <p style={{ marginBottom: "20px" }}>
-          〒999-3751 山形県東根市若木通り1-13
-          <br />
-          ℡:090-8251-5523
-          <br />
-          E-mail:mitsubachi-kajuen@ae.auone-net.jp
-          <br />
+        {/* ナビゲーション */}
+        <nav
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            gap: "18px",
+            fontSize: "14px",
+          }}
+        >
+          <Link href="/" style={linkStyle}>
+            Home
+          </Link>
+          <Link href="/story" style={linkStyle}>
+            Story
+          </Link>
+          <Link href="/fruits" style={linkStyle}>
+            Fruits
+          </Link>
+          <Link href="/order" style={linkStyle}>
+            Order
+          </Link>
+          <Link href="/contact" style={linkStyle}>
+            Contact
+          </Link>
+          <Link href="/legal" style={linkStyle}>
+            特定商取引法に基づく表示
+          </Link>
+        </nav>
 
-        </p>
-
-        <p style={{ color: "#888" }}>
-          © 2026 みつばち果樹園 All Rights Reserved.
+        <p
+          style={{
+            margin: 0,
+            color: "#777",
+            fontSize: "13px",
+            lineHeight: 1.8,
+          }}
+        >
+          山形県東根市から、季節の果物をお届けします。
+          <br />
+          © {new Date().getFullYear()} みつばち果樹園
         </p>
       </div>
     </footer>
   );
 }
+
+const linkStyle: React.CSSProperties = {
+  color: "#555",
+  textDecoration: "none",
+  fontWeight: 700,
+};
